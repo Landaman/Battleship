c99e81c09e3c4079bb75a41d18a0fd8cf9d6ccef
Landaman <irswright13@gmail.com>
2019-05-21 12:42:32 -0400

    Initial UI framework

--- UI Control.py
+++ UI Control.py
@@ -1,1 +1,30 @@
 # This is the UI Controls. It will also handle player side input
+from tkinter import *
+
+# Main UI elements
+main = Tk
+menu = Frame(main)
+howTo = Frame(main)
+settings = Frame(main)
+playerBoard = Frame(main)
+AIBoard = Frame(main)
+
+# Menu UI elements
+playButton = Button(menu, text="Play")
+howToButton = Button(menu, text="How to Play")
+settingsButton = Button(menu, text="Settings")
+
+# How To UI elements
+howToText = Text(howTo)
+howToText.insert()  # TODO: Insert the how to text here.
+howToText.configure(state=DISABLED)
+
+# Settings UI elements
+settingsBox = Listbox(settings, selectmode=SINGLE)
+settingsBox.insert(1, "Easy")
+settingsBox.insert(2, "Normal")
+settingsBox.insert(3, "Hard")
+settingsBox.see(2)
+
+# Player Board UI elements
+
