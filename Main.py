5d886f1de799366136d38f690dcc7741f0d0f951
Landaman <irswright13@gmail.com>
2019-05-21 12:42:55 -0400

    Initial general game framework

--- Main.py
+++ Main.py
@@ -1,1 +1,8 @@
 # This is the general program flow and game execution
+BASE_BOARD = []
+for i in range(10):  # This creates the stock board
+    BASE_BOARD.append([])
+    for j in range(10):
+        BASE_BOARD[i].append(" ")
+AIBoard = BASE_BOARD
+playerBoard = BASE_BOARD
