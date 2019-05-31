6e243da13bc4e21c5abe21d146f95ccc133a7829
tdonnelly21 <50834635+tdonnelly21@users.noreply.github.com>
2019-05-31 08:42:37 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,3 +10,23 @@ for i in range(10):  # This creates the stock board
         BASE_BOARD[i].append(" ")
 AIBoard = BASE_BOARD
 playerBoard = BASE_BOARD
+
+#player turn
+def pturn(){
+ptarget = input("Enter the coorfinates of your desired target")
+    if ptarget = full{
+        #(show hit marker on coordinate)
+        print("You hit a ship!")
+    }
+    else{
+        #(show miss marker on coordinate)
+        print("you missed")
+    }
+    #(begin AI turn)
+    }
+    
+    #AI turn
+def aiturn(){
+    
+
+}
