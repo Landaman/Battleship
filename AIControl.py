7d163820a8109d3f7e40cc36953bdd82248af1d9
tdonnelly21 <50834635+tdonnelly21@users.noreply.github.com>
2019-06-03 09:43:21 -0400

    Update AIControl.py

--- AIControl.py
+++ AIControl.py
@@ -1,1 +1,16 @@
 # This is the AI Control, both board setup and actual moves
+import random
+
+#AI turn based on same system as player turn
+def aiturn(){
+aitarget = randint(1,10), randint(1,10)
+    if aitarget = full{
+        #(show hit marker on aitarget)
+        print("Your ship at", aitarget, "has been hit!")
+    }
+    else{
+        #(show miss marker on coordinate)
+        print("The enemy missed.")
+    }
+    pturn()
+    }
