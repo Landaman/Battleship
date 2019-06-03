1d2093d97549ec6988bc28bbba87430f351d833d
tdonnelly21 <50834635+tdonnelly21@users.noreply.github.com>
2019-06-03 09:43:16 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,16 +11,16 @@ for i in range(10):  # This creates the stock board
 AIBoard = BASE_BOARD
 playerBoard = BASE_BOARD
 
-#player turn is set up on a system that checks if a coord has a ship in it (ship on coord -> "full")
+#player turn is set up on a system that checks if a coord has a ship on it (ship on coord -> "full")
 def pturn(){
-ptarget = input("Enter the coorfinates of your desired target")
+ptarget = input("Select where on the grid you would like to fire.")
     if ptarget = full{
         #(show hit marker on coordinate)
         print("You hit a ship!")
     }
     else{
         #(show miss marker on coordinate)
-        print("you missed")
+        print("You missed.")
     }
-    #(begin AI turn)
+    aiturn()
     }
